26e0a51f532fe5d2d2a57ee6e8849acbed896c18
deepali <deepaalipatil@gmail.com>
2022-01-04 22:26:41 +0530

    ADDED : Password should contain atleast 1 Uppercase character

--- src/com/bridgelabz/regex/RegexRegistration.java
+++ src/com/bridgelabz/regex/RegexRegistration.java
@@ -45,12 +45,12 @@ public class RegexRegistration {
 	
 	
 	public static  boolean userPassword(String password) {
-	    String Passreg="^(?=.*[0-9])(?=.*[A-Z])([0-9A-Za-z]){8,}$"; // Rule-3 Atleast one Numeric Number in Password
+	    String Passreg="^(?=.*[A-Z])([0-9A-Za-z]){8,}$"; // Rule-2 Atleast one Uppercase in Password
 	    
 		Pattern pattern =Pattern.compile(Passreg);
 		Matcher matcher = pattern.matcher(password);
 		
-	System.out.println(matcher.matches());
+	  System.out.println(matcher.matches());
 		return true;
     }
 	
