6270f69f1a1c5c4f2fc29e3a3632fa9f16fa8575
deepali <deepaalipatil@gmail.com>
2022-01-04 23:14:28 +0530

    ADDED : check all sample emails

--- src/com/bridgelabz/regex/RegexRegistration.java
+++ src/com/bridgelabz/regex/RegexRegistration.java
@@ -33,7 +33,7 @@ public class RegexRegistration {
 	
 	public static  boolean userEmail(String email) {
 		
-		 String emailreg = "^[A-Za-z0-9_-]+([.][A-Za-z0-9_-]+)@[A-Za-z]+[A-Za-z]+([.][A-Za-z]+)$";
+		 String emailreg = "^[A-Za-z0-9-]+(.[A-Za-z0-9-]+)*@[^_\\W]+(.[^_\\W]+)?(?=(.[^_\\W]{3,}$|.[a-zA-Z]{2}$)).*$";//UC-09 For Sample Emails
 		 Pattern pattern = Pattern.compile(emailreg);
 		 Matcher matcher = pattern.matcher(email);
 		 
