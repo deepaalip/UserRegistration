862c712c04a1c71595f604f501802a95ef6d29c1
deepaalip <95680526+deepaalip@users.noreply.github.com>
2022-01-05 23:40:24 +0530

    UPDATED: User registration regex program

--- src/com/bridgelabz/regex/RegexRegistration.java
+++ src/com/bridgelabz/regex/RegexRegistration.java
@@ -5,53 +5,39 @@ import java.util.regex.Pattern;
 
 public class RegexRegistration {
 	
-	public   boolean userFirstName(String firstName) {
-
-		String FirstName= "^[A-Z][a-z]{3,}";
-		Pattern pattern =Pattern.compile(FirstName);
-		Matcher matcher = pattern.matcher(firstName);
+	private static final String FirstNameRegex= "^[A-Z][a-z]{2,}";
+	private static final String LastNameRegex= "^[A-Z][a-z]{2,}";
+	private static final String EmailRegex = "^[A-Za-z0-9_-]+([.][A-Za-z0-9_-]+)@[A-Za-z]+[A-Za-z]+([.][A-Za-z]+)";
+	private static final String PhoneNoRegex = "^[0-9]{2}[0-9]{10}";
+	private static final String PasswordRegex="^(?=.*[a-z])(?=."+ "*[A-Z])(?=.*[0-9])"+ "(?=.*[-+_!@#$%^&*., ?]).+$";
 	
-	    System.out.println(matcher.matches());
-		return true;		
+	public  boolean userFirstName(String firstName) {
+
+		return Pattern.matches(FirstNameRegex, firstName);	
     
-
-	
-	}
-
+   }
 	
 	public  boolean userLastName(String lastName) {
 
-		String LastName= "^[A-Z][a-z]{3,}";
-		Pattern pattern =Pattern.compile(LastName);
-		Matcher matcher = pattern.matcher(lastName);
-	
-	System.out.println(matcher.matches());
-		return true;		
+		return Pattern.matches(LastNameRegex, lastName);		
 
+	}
+
+	public boolean userEmail(String email) {
+		
+		return Pattern.matches(EmailRegex, email);
+	}
+		
+    public boolean userPhoneNo(String phone) {
+		
+		return Pattern.matches(PhoneNoRegex, phone);
+	}
+		
+    public boolean userPassword(String password) {
+		
+		return Pattern.matches(PasswordRegex, password);
 	}
 	
 	
-	public  boolean userEmail(String email) {
-		
-		 String emailreg = "^[A-Za-z0-9-]+(.[A-Za-z0-9-]+)*@[^_\\W]+(.[^_\\W]+)?(?=(.[^_\\W]{3,}$|.[a-zA-Z]{2}$)).*$";//UC-09 For Sample Emails
-		 Pattern pattern = Pattern.compile(emailreg);
-		 Matcher matcher = pattern.matcher(email);
-		 
-		 System.out.println(matcher.matches());
-			return true;		
-
-	}
-	
-	
-	
-	public  boolean userPassword(String password) {
-	    String Passreg="^(?=.*[a-z])(?=."+ "*[A-Z])(?=.*[0-9])"+ "(?=.*[-+_!@#$%^&*., ?]).+$";
-	    
-		Pattern pattern =Pattern.compile(Passreg);
-		Matcher matcher = pattern.matcher(password);
-		
-	  System.out.println(matcher.matches());
-		return true;
-    }
 	
 }
